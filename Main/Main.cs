344ab047539405696d972294da59bf4d0731bb90
corbinss <corbinlmiller2@gmail.com>
2022-02-26 23:16:30 -0600

    Revert "fix nocwip UwU i sowwy for fawing so mawy timws"
    This reverts commit 3ee743462ccc441ffb0b258351987b1cbe4b6ce8.

--- Main/Main.cs
+++ Main/Main.cs
@@ -1481,8 +1481,8 @@ namespace PendulumClient.Main
                     }
                     if (NoClip == true)
                     {
-                        Vector3 vector = currentPlayer.gameObject.transform.position - VRCTrackingManager.Method_Public_Static_Vector3_4();
-                        Quaternion quaternion = currentPlayer.gameObject.transform.rotation * Quaternion.Inverse(VRCTrackingManager.Method_Public_Static_Quaternion_2());
+                        Vector3 vector = currentPlayer.gameObject.transform.position - VRCTrackingManager.Method_Public_Static_Vector3_0();
+                        Quaternion quaternion = currentPlayer.gameObject.transform.rotation * Quaternion.Inverse(VRCTrackingManager.Method_Public_Static_Quaternion_0());
                         VRCTrackingManager.Method_Public_Static_Void_Vector3_Quaternion_1(vector, quaternion);
                     }
                 }
